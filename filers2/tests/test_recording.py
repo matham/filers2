f878f7daf9411ef30f8792a2a697ea03d8e74777
Matthew Einhorn <matt@einhorn.dev>
2022-03-30 00:37:50 -0400

    Make tests explicitly fail.

--- filers2/tests/test_recording.py
+++ filers2/tests/test_recording.py
@@ -1,6 +1,18 @@
+import time
 
 
 def test_player():
     from filers2.recording import FilersPlayer
     player = FilersPlayer()
+    thor = player.thor_player
+
+    if thor.is_available:
+        ts = time.perf_counter()
+        while not thor.process_connected and time.perf_counter() - ts < 20:
+            time.sleep(1)
+
+        if not thor.process_connected:
+            thor.stop_cam_process(join=True, kill_delay=5)
+            raise TimeoutError
+
     player.clean_up()
